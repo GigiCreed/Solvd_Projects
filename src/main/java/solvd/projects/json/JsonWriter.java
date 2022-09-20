ec3680f74f33022fab4386eaab7e77278aad3412
GigiMamaladze <gigimamaladze13@gmail.com>
2022-09-20 21:23:05 +0400

    removed imports whith I did not use

--- src/main/java/solvd/projects/json/JsonWriter.java
+++ src/main/java/solvd/projects/json/JsonWriter.java
@@ -1,6 +1,5 @@
 package solvd.projects.json;
 
-import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,7 +7,6 @@ import solvd.projects.json.classes.Students;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
