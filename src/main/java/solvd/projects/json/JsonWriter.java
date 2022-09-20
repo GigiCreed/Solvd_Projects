ad00f7ac3c978599a70a9352a4e9b536c10068d7
GigiMamaladze <gigimamaladze13@gmail.com>
2022-09-20 21:00:07 +0400

    modifed JsonWriter Class

--- src/main/java/solvd/projects/json/JsonWriter.java
+++ src/main/java/solvd/projects/json/JsonWriter.java
@@ -16,22 +16,21 @@ public class JsonWriter {
     private static final Logger LOGGER = LogManager.getLogger(JsonWriter.class);
     public static void main(String[] args) {
 
-        Students students = new Students("gigi","mamaladze",new Date(10140768000000L),555,4);
-        Students students2 = new Students("Nika","mamaladze",new Date(10140768000000L),555,4);
-
-        List<Students> studentsList = new ArrayList<>();
-        studentsList.add(students);
-        studentsList.add(students2);
-
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JavaType javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, Students.class);
-            objectMapper.writeValue(new File("src/main/resources/jsonfiles/studentsWriter.json"),studentsList);
-
-
+            objectMapper.writeValue(new File("src/main/resources/jsonfiles/studentsWriter.json"),getStudents());
+            LOGGER.info("File is Created");
         }catch (IOException ex){
             LOGGER.error(ex);
         }
+    }
+    public static List<Students> getStudents() {
+        return List.of(
+        new Students("gigi", "mamaladze", new Date(10140768000000L), 555, 4),
+        new Students("Nika", "mamaladze", new Date(10140768000000L), 555, 4),
+        new Students("Irakli", "mamaladze", new Date(10140768000000L), 555, 4)
+      );
     }
 
 }
